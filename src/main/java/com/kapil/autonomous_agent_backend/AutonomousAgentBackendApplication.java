d5c4b4129257c74dbc39e88e4c255b392fb29f06
QL-Kapil-Kant <kapil.kant@quokkalabs.com>
2026-07-10 14:49:03 +0530

    first commit

---
+++ src/main/java/com/kapil/autonomous_agent_backend/AutonomousAgentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kapil.autonomous_agent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutonomousAgentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutonomousAgentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kapil/autonomous_agent_backend/AutonomousAgentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapil.autonomous_agent_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutonomousAgentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
